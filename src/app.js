010259bb7dba60ff5578fbdc5559fa3a4fc2d1f1
LethaL <akubaku444@gmail.com>
2022-04-27 23:45:01 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("hbs");
+
+require("./db/conn")
+const Register = require("./models/registers");
+
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partials_path);
+
+app.get("/", (req, res)=>{
+    res.render("index");
+})
+
+app.get("/register", (req, res)=>{
+    res.render("register");
+})
+
+app.get("/login", (req, res)=>{
+    res.render("login");
+})
+
+app.post("/register", async (req, res)=>{
+    try{
+        const password = req.body.password;
+        const cpassword = req.body.confirmpassword;
+        if(password===cpassword){
+            const registerPeople = new Register({
+                firstname:req.body.firstname,
+                lastname:req.body.lastname,
+                email:req.body.email,
+                gender:req.body.gender,
+                phone:req.body.phone,
+                age:req.body.age,
+                password:password,
+                confirmpassword:cpassword
+            })
+            const registered = await registerPeople.save();
+            res.status(201).render("index");
+        }else{
+            res.send("passwords are not matching");
+        }
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+app.post("/login", async (req, res)=>{
+    try{
+        const email = req.body.email;
+        const password = req.body.password;
+        
+        const useremail = await Register.findOne({email:email});
+        if(useremail.password===password){
+            res.status(201).render("index");
+        }else{
+            res.send("password are not matching");
+        }
+    }catch(e){
+        res.status(400).send("Invalid Email");
+    }
+})
+
+app.listen(port, ()=>{
+    console.log(`server is listening on port ${port}`);
+})
